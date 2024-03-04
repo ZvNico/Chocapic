ac6958eeab75d0190a53e57d6f8537e9994fb4f7
Laurie Bral <laurie.bral@efrei.net>
2024-03-04 15:34:53 +0100

    Modify verification Add an employee with long value

--- end_to_end_tests/index.test.ts
+++ end_to_end_tests/index.test.ts
@@ -1,83 +1,81 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 import EmployeeCreateFormPage from "./EmployeeCreateFormPage";
-import {ResetDatabasePage} from "./ResetDatabasePage";
+import { ResetDatabasePage } from "./ResetDatabasePage";
 import EmployeesPage from "./EmployeesPage";
-import {Browser, chromium, Page} from "playwright";
+import { Browser, chromium, Page } from "playwright";
 
 let browser: Browser;
 let page: Page;
 
 test.beforeAll(async () => {
-    browser = await chromium.launch();
-    const context = await browser.newContext();
-    page = await context.newPage();
+  browser = await chromium.launch();
+  const context = await browser.newContext();
+  page = await context.newPage();
 
-    const resetDatabasePage = new ResetDatabasePage(page);
-    await resetDatabasePage.navigate();
-    await resetDatabasePage.resetDatabase();
+  const resetDatabasePage = new ResetDatabasePage(page);
+  await resetDatabasePage.navigate();
+  await resetDatabasePage.resetDatabase();
 });
 
 test.afterAll(async () => {
-    await browser.close();
+  await browser.close();
 });
-test('home page', async ({page}) => {
-    await page.goto('https://c.hr.dmerej.info')
-    await expect(page).toHaveTitle(/Home/);
-})
+test("home page", async ({ page }) => {
+  await page.goto("https://c.hr.dmerej.info");
+  await expect(page).toHaveTitle(/Home/);
+});
 
 // Test for recreate the issue Add an employee with long value in inputs
-test("Add an employee with long value in inputs", async ({page}) => {
-    const employeeCreateFormPage = new EmployeeCreateFormPage(page);
+test("Add an employee with long value in inputs", async ({ page }) => {
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
+  const employeesPage = new EmployeesPage(page);
 
-    await employeeCreateFormPage.navigate();
-    await employeeCreateFormPage.fillForm({
-        zipCode: "1".repeat(50),
-    });
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm({
+    zipCode: "1".repeat(50),
+  });
 
-    // Response we are expecting
-    const responsePromise = page.waitForResponse(
-        (response) => response.status() === 500
-    );
+  await employeeCreateFormPage.submit();
 
-    await employeeCreateFormPage.submit();
-
-    // Get the response and check the status
-    const response = await responsePromise;
-    expect(response.status()).toBe(500);
+  await employeesPage.navigate();
+  const isUserPresent = await employeesPage.isUserPresent(
+    "Laurie",
+    "test.zipCode@gmail.com"
+  );
+  expect(isUserPresent).toBe(true);
 });
 
-test('create a user', async ({page}) => {
-    const employeesPage = new EmployeesPage(page)
-    const employeeCreateFormPage = new EmployeeCreateFormPage(page)
+test("create a user", async ({ page }) => {
+  const employeesPage = new EmployeesPage(page);
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
 
-    await employeeCreateFormPage.navigate()
-    await employeeCreateFormPage.fillForm({
-        name: 'John Doe',
-        email: 'john.doe@gmail.com'
-    })
-    await employeeCreateFormPage.submit()
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm({
+    name: "John Doe",
+    email: "john.doe@gmail.com",
+  });
+  await employeeCreateFormPage.submit();
 
-    await employeesPage.navigate()
-    const isUserPresent = await employeesPage.isUserPresent('John Doe', 'john.doe@gmail.com')
-    expect(isUserPresent).toBe(true)
-})
+  await employeesPage.navigate();
+  const isUserPresent = await employeesPage.isUserPresent(
+    "John Doe",
+    "john.doe@gmail.com"
+  );
+  expect(isUserPresent).toBe(true);
+});
 
-test(' Employees are wipe when their team is deleted', async ({page}) => {
+test(" Employees are wipe when their team is deleted", async ({ page }) => {});
 
-    }
-)
+test("Add employees with the same email", async ({ page }) => {
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
 
-test('Add employees with the same email', async ({page}) => {
-        const employeeCreateFormPage = new EmployeeCreateFormPage(page)
+  //Create the first employee
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+  await employeeCreateFormPage.submit();
 
-        //Create the first employee
-        await employeeCreateFormPage.navigate()
-        await employeeCreateFormPage.fillForm()
-        await employeeCreateFormPage.submit()
-
-        //Create the second employee with the same email
-        await employeeCreateFormPage.navigate()
-        await employeeCreateFormPage.fillForm()
-        await employeeCreateFormPage.submit()
-    }
-)
+  //Create the second employee with the same email
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+  await employeeCreateFormPage.submit();
+});
