cd9fd97254645360ab4c5db9d12eb79294bf14c4
FarahFrgne05 <farah.ferguene@efrei.net>
2024-03-04 14:36:59 +0100

    test - Add employees with the same email

--- end_to_end_tests/index.test.ts
+++ end_to_end_tests/index.test.ts
@@ -18,3 +18,20 @@ test(' Employees are wipe when their team is deleted', async ({page}) => {
 
     }
 )
+
+test('Add employees with the same email', async ({page}) => {
+    const employeeCreateFormPage = new EmployeeCreateFormPage(page)
+
+    //Create the first employee
+    await employeeCreateFormPage.navigate()
+    await employeeCreateFormPage.fillForm()
+    await employeeCreateFormPage.submit()
+
+    //Create the second employee with the same email
+    await employeeCreateFormPage.navigate()
+    await employeeCreateFormPage.fillForm()
+    await employeeCreateFormPage.submit()
+
+
+    }
+)
