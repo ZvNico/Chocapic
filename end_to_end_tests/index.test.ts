f007069d33d548bcde2ff8c802aa56a8ca7ac42f
Laurie Bral <laurie.bral@efrei.net>
2024-03-04 14:54:45 +0100

    Second creation of the add an employee with long value in inputs

--- end_to_end_tests/index.test.ts
+++ end_to_end_tests/index.test.ts
@@ -1,42 +1,40 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 import EmployeeCreateFormPage from "./EmployeeCreateFormPage";
-import {ResetDatabasePage} from "./ResetDatabasePage";
+import { ResetDatabasePage } from "./ResetDatabasePage";
 import EmployeesPage from "./EmployeesPage";
 
-test.beforeAll(async ({page}) => {
-    const resetDatabasePage = new ResetDatabasePage(page)
-    await resetDatabasePage.navigate()
-    await resetDatabasePage.resetDatabase()
-})
+test.beforeAll(async ({ page }) => {
+  const resetDatabasePage = new ResetDatabasePage(page);
+  await resetDatabasePage.navigate();
+  await resetDatabasePage.resetDatabase();
+});
 
-test('home page', async ({page}) => {
-    await page.goto('https://c.hr.dmerej.info')
-    await expect(page).toHaveTitle(/Home/);
-})
+test("home page", async ({ page }) => {
+  await page.goto("https://c.hr.dmerej.info");
+  await expect(page).toHaveTitle(/Home/);
+});
 
-test('create a user', async ({page}) => {
-    const employeeCreateFormPage = new EmployeeCreateFormPage(page)
-    const employeesPage = new EmployeesPage(page)
-    await employeeCreateFormPage.navigate()
-    await employeeCreateFormPage.fillForm()
-})
+test("create a user", async ({ page }) => {
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
+  const employeesPage = new EmployeesPage(page);
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+});
 
-test(' Employees are wipe when their team is deleted', async ({page}) => {
-    
-    }
-)
+test(" Employees are wipe when their team is deleted", async ({ page }) => {});
 
-test('Add employees with the same email', async ({page}) => {
-        const employeeCreateFormPage = new EmployeeCreateFormPage(page)
+test("Add an employee with long value in inputs", async ({ page }) => {});
 
-        //Create the first employee
-        await employeeCreateFormPage.navigate()
-        await employeeCreateFormPage.fillForm()
-        await employeeCreateFormPage.submit()
+test("Add employees with the same email", async ({ page }) => {
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
 
-        //Create the second employee with the same email
-        await employeeCreateFormPage.navigate()
-        await employeeCreateFormPage.fillForm()
-        await employeeCreateFormPage.submit()
-    }
-)
+  //Create the first employee
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+  await employeeCreateFormPage.submit();
+
+  //Create the second employee with the same email
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+  await employeeCreateFormPage.submit();
+});
