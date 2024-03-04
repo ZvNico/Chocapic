4b1be8fadbe765ac1e62ad1f8530179128290cb7
Laurie Bral <laurie.bral@efrei.net>
2024-03-04 14:33:16 +0100

    Creation Of The test Add an employee with long value in inputs

--- end_to_end_tests/index.test.ts
+++ end_to_end_tests/index.test.ts
@@ -1,20 +1,17 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from "@playwright/test";
 import EmployeeCreateFormPage from "./EmployeeCreateFormPage";
 
+test("home page", async ({ page }) => {
+  await page.goto("https://c.hr.dmerej.info");
+  await expect(page).toHaveTitle(/Home/);
+});
 
-test('home page', async ({page}) => {
-    await page.goto('https://c.hr.dmerej.info')
-    await expect(page).toHaveTitle(/Home/);
-})
+test("create a user", async ({ page }) => {
+  const employeeCreateFormPage = new EmployeeCreateFormPage(page);
+  await employeeCreateFormPage.navigate();
+  await employeeCreateFormPage.fillForm();
+});
 
-test('create a user', async ({page}) => {
-    const employeeCreateFormPage = new EmployeeCreateFormPage(page)
-    await employeeCreateFormPage.navigate()
-    await employeeCreateFormPage.fillForm()
+test(" Employees are wipe when their team is deleted", async ({ page }) => {});
 
-})
-
-test(' Employees are wipe when their team is deleted', async ({page}) => {
-
-    }
-)
+test("Add an employee with long value in inputs", async ({ page }) => {});
